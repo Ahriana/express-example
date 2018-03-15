d7de521ac04b497f32f3777711a9216d54d8e325
RosieCode95 <rosiecode@outlook.com>
2018-03-15 23:06:50 +0000

    clusters

--- app.js
+++ app.js
@@ -10,37 +10,46 @@ const users = require('./routes/users');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+module.exports = (cluster) => {
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'pug');
 
-app.use('/', index);
-app.use('/users', users);
+    // uncomment after placing your favicon in /public
+    // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+    // can add local ws variables here
+    app.use((req, res, next) => { 
+        res.locals.workerid = cluster.worker.id
+        next();
+    });
 
-// error handler
-app.use((err, req, res, next) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    app.use('/', index);
+    app.use('/users', users);
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+    // catch 404 and forward to error handler
+    app.use((req, res, next) => {
+        const err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
 
-module.exports = app;
+    // error handler
+    app.use((err, req, res, next) => {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+        // render the error page
+        res.status(err.status || 500);
+        res.render('error');
+    });
+
+    return app;
+}
--- index.js
+++ index.js
@@ -1,50 +1,14 @@
-const app = require('./app.js');
-const debug = require('debug')('demo:server');
-const http = require('http');
+const cluster = require('cluster');
 
-// Get port from environment and store in Express.
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+if (cluster.isMaster) {
+    const cpuCount = require('os').cpus().length;
 
-// Create HTTP server.
-const server = http.createServer(app);
+    for (var i = 0; i < cpuCount; i += 1) { cluster.fork(); }
+    cluster.on('exit', (worker) => {
+        console.log(`cluster ${worker.id} died, spawning new cluster`);
+        cluster.fork();
+    });
 
-// Listen on provided port, on all network interfaces.
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-// Normalize a port into a number, string, or false.
-function normalizePort(val) {
-    const port = parseInt(val, 10);
-    if (isNaN(port)) { return val; } // named pipe
-    if (port >= 0) { return port; } // port number
-    return false;
-}
-
-// Event listener for HTTP server "error" event.
-function onError(error) {
-    if (error.syscall !== 'listen') { throw error; }
-    const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
-
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(`${bind} requires elevated privileges`);
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(`${bind} is already in use`);
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
-
-// Event listener for HTTP server "listening" event.
-function onListening() {
-    const addr = server.address();
-    const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
-    debug(`Listening on ${bind}`);
+} else {
+    require('./server')(cluster);
 }
